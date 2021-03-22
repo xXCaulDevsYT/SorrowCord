86ab58e47dfd8c50a95cf1932630d05a4cd980bc
JustSorrow <34898438+xXCaulDevsYT@users.noreply.github.com>
2021-03-22 13:24:57 -0500

    Create i18n.js

---
+++ src/SorrowCord/apis/i18n.js
@@ -0,0 +1,53 @@
+const { getModule, i18n } = require('sorrowcord/webpack');
+const { API } = require('sorrowcord/entities');
+const strings = require('../../../i18n');
+const overrides = require('../../../i18n/overrides');
+
+module.exports = class I18nAPI extends API {
+  constructor () {
+    super();
+    this.messages = {};
+    this.locale = null;
+    this.loadAllStrings(strings);
+    this.loadAllStrings(overrides);
+  }
+
+  startAPI () {
+    getModule([ 'locale', 'theme' ]).then(module => {
+      this.locale = module.locale;
+      module.addChangeListener(() => {
+        if (module.locale !== this.locale) {
+          this.locale = module.locale;
+          i18n.loadPromise.then(() => this.addSorrowcordStrings());
+        }
+      });
+      this.addSorrowcordStrings();
+    });
+  }
+
+  addPowercordStrings () {
+    Object.assign(i18n._proxyContext.messages, this.messages[this.locale]);
+    Object.assign(i18n._proxyContext.defaultMessages, this.messages['en-US']);
+
+    // begone annoying warning
+    [ 'messages', 'defaultMessages' ].forEach(obj => {
+      Object.keys(i18n._proxyContext[obj]).filter(key => key.startsWith('SELF_XSS')).forEach(key => delete i18n._proxyContext[obj][key]);
+    });
+  }
+
+  loadAllStrings (strings) {
+    Object.keys(strings).forEach(locale => this.loadStrings(locale, strings[locale]));
+  }
+
+  loadStrings (locale, strings) {
+    if (!this.messages[locale]) {
+      this.messages[locale] = strings;
+    } else {
+      this.messages[locale] = {
+        ...this.messages[locale],
+        ...strings
+      };
+    }
+    this.addSorrowcordStrings();
+  }
+};
